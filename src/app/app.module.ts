54f529e435a5c9254e0aabf8eedbfbd1f213d2c5
Adrian Flisc <flisc.adrian@sonrisa.hu>
2022-12-07 12:39:51 +0200

    lister and add implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddProductComponent } from './components/add-product/add-product.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ServicesListComponent } from './components/services-list/services-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddProductComponent
+    AddProductComponent,
+    HeaderComponent,
+    ServicesListComponent
   ],
   imports: [
     BrowserModule,
